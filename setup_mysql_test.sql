3e3298da52adff78bc7da5d52c3045af39256fc6
Adereti Adebobola <emmanueladebobola132@gmail.com>
2022-09-14 15:54:46 -0700

    commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,5 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
-
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- sets up the database 'hbnb_test_db'
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
