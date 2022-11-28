ed7bf305e55334daa906e0f415a9c265ef3a83e6
Gladys Godwin <gladysgodwin79@gmail.com>
2022-11-28 17:43:25 +0100

    fixed bugs

--- models/user.py
+++ models/user.py
@@ -22,7 +22,17 @@ class User(BaseModel, Base):
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")''
+    reviews = relationship("Review", backref="user", cascade="delete")
+
+else:
+        email = ""
+        password = ""
+        last_name = ""
+        first_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initalize user"""
+        super().__init__(*args, **kwargs)
